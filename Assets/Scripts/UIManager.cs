18b367d0486f42944a69430aa9b02a35fbbeddaa
sancrack235 <sancrack267@gmail.com>
2026-04-24 14:23:19 -0300

    Trabajo con ambas consignas terminado

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_InputField inputNumero;
+    public TextMeshProUGUI outputNumero;
+    public int numCalculo;
+    public TextMeshProUGUI advertenciaCartel;
+    public int tempNum;
+
+
+    void Start()
+    {
+        advertenciaCartel.gameObject.SetActive(false);
+        outputNumero.gameObject.SetActive(false);
+        tempNum = 0;
+    }
+
+    public void RestarUno() 
+    {
+        if (tempNum < 1) {
+            numCalculo = int.Parse(inputNumero.text);
+        }
+        if (numCalculo > 0) 
+        {
+            tempNum = tempNum + 1;
+            advertenciaCartel.gameObject.SetActive(false);
+            outputNumero.gameObject.SetActive(true);
+
+            numCalculo = numCalculo - 1;
+            
+            outputNumero.text = numCalculo.ToString();
+        }
+        else 
+        {
+            advertenciaCartel.gameObject.SetActive(true);
+        }
+        if ((numCalculo + tempNum) != int.Parse(inputNumero.text)) {
+            numCalculo = 0;
+            tempNum = 0;
+        }
+        Debug.Log(numCalculo + tempNum);
+    }
+}
